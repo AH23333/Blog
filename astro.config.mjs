d0e64ceb45a05dfef3b41dbff775b61021c083ef
AH <3469726343@qq.com>
2026-06-17 23:06:16 +0800

    修改部署配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://www.cubeyond.net/",
+  // GitHub Pages 项目站点 URL
+  site: "https://AH23333.github.io/Blog/",
   devToolbar: {
     enabled: false
   },
