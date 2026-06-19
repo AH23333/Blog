6c245fb6b9f699c25a92a24251fa9d222d5d7d26
AH <3469726343@qq.com>
2026-06-19 17:31:18 +0800

    继续修改配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // Vercel 部署（无 base path 问题）
   site: "https://www.cubeyond.net/",
   devToolbar: {
     enabled: false
